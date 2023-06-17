a110ee47da69069c519ffe4f86dbcd69c3fd62e2
aktaruzzaman-shaon <aktaruzzamanshaon69@gmail.com>
2023-06-18 00:04:00 +0600

    Modidy footer

--- src/Home/Footer/Footer.js
+++ src/Home/Footer/Footer.js
@@ -4,25 +4,25 @@ const Footer = () => {
     return (
         <div className='container'>
             <div className="row">
-                <div className="col">
+                <div className="col-md-3 col-sm-6">
                     <div>
                         <h4 className='text-start'>Support</h4>
                         <p className='text-start'>Add your trip dates to get the cancellation details for this stay.</p>
                     </div>
                 </div>
-                <div className="col">
+                <div className="col-md-3 col-sm-6">
                     <div>
                         <h4 className='text-start'>Community</h4>
                         <p className='text-start'>Add your trip dates to get the cancellation details for this stay.</p>
                     </div>
                 </div>
-                <div className="col">
+                <div className="col-md-3 col-sm-6">
                     <div>
                         <h4 className='text-start'>Hsoiting</h4>
                         <p className='text-start'>Add your trip dates to get the cancellation details for this stay.</p>
                     </div>
                 </div>
-                <div className="col">
+                <div className="col-md-3 col-sm-6">
                     <div>
                         <h4 className='text-start'>Aibng</h4>
                         <p className='text-start'>Add your trip dates to get the cancellation details for this stay.</p>
@@ -34,21 +34,14 @@ const Footer = () => {
 
             <div>
                 <div className="container ">
-                    {/* Social Media part */}
-                    <div className="row">
-                        <div className="col-12 mt-5">
-                        </div>
-                    </div>
-
-
                     {/* Copy right part */}
                     <div className='row'>
-                        <div className='col-sm-12 col-md-6 col-lg-6 copy-right'><p className='text-start'>&copy; 2023 023 Airbnb, Inc.</p></div>
-                        <div className='col-sm-12 col-md-6 col-lg-6 termCoockie-Style text-end'>
-                            <a href="" cla>Terms</a>
-                            <a href="">Cookie Policy</a>
-                            <a href="">Terms</a>
-                            <a href="">Privacy</a>
+                        <div className='col-sm-12 col-md-6 col-lg-6 copy-right'><p >&copy; 2023 023 Airbnb, Inc.</p></div>
+                        <div className='col-sm-12 col-md-6 col-lg-6 d-sm-text-start'>
+                            <a href="" className='text-black text-decoration-none'>Terms</a>
+                            <a href="" className='text-black text-decoration-none'>Cookie Policy</a>
+                            <a href="" className='text-black text-decoration-none'>Terms</a>
+                            <a href="" className='text-black text-decoration-none'>Privacy</a>
                         </div>
                     </div>
                 </div>
